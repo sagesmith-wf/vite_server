11d84c745adfd7a9b457ad6c39ea788aa8a33402
Sage.Smith <sage.smith@workiva.com>
2016-12-31 01:10:46 -0700

    Test docker

--- docker/dockerize.sh
+++ docker/dockerize.sh
@@ -1,9 +1,10 @@
+#!/usr/bin/env bash
 set -ex
 
 NAME=vite-server
 TAG=${1:-test}
 
-./gradlew build
-mv build/libs/$NAME-*.jar docker/$NAME.jar
+#./gradlew build
+#mv build/libs/$NAME-*.jar docker/$NAME.jar
 docker build docker/ -t "${NAME}:${TAG}"
-rm docker/$NAME.jar
+#rm docker/$NAME.jar
